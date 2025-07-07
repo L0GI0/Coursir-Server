512a166393361c2a0ecc3b8db26dff18b0e1e106
Mike Smith <logio@Mikes-MacBook-Pro.local>
2025-07-07 17:31:50 +0200

    release: 1.0.0 create express server | connect to dynamoDB

---
+++ src/controllers/courseController.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from 'express';
+import Course from '../models/courseModel';
+
+//Gets courses of specific category or all courses if category not specified
+export const listCourses = async(
+    req: Request,
+    res: Response
+): Promise<void> => {
+    const { category } = req.query;
+
+    try {
+        const courses = category && category !== 'all'
+        ? await Course.scan("category").eq(category).exec()
+        : await Course.scan().exec();
+        res.json({ message: "Courses retrieved successfully", data: courses })
+    } catch (error) {
+        res.status(500).json({ message: "Error revrieving courses", error})
+    }
+}
+
+//Grab single course
+export const getCourse = async (req: Request, res: Response): Promise<void> => {
+    const { courseId } = req.params;
+
+    try {
+
+        console.log(`Course id = ${courseId}`);
+
+        const course = await Course.get(courseId);
+
+        if(!course) {
+            res.status(404).json({ message: "Course not found"});
+            return;
+        }
+
+        res.json({ message: "Course retrieved successfully", data: course })
+    } catch (error) {
+        res.status(500).json({ message: "Error revrieving course", error})
+    }  
+}
