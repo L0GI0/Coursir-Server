57e57b2ae08b23e0020c42b59b2913d41fa3ee5b
Michal <logiocorp@gmail.com>
2025-07-12 11:39:05 +0200

    feat: exted courses api to full CRUD

--- src/routes/courseRoutes.ts
+++ src/routes/courseRoutes.ts
@@ -1,10 +1,16 @@
 import express from "express";
-import { listCourses, getCourse } from "../controllers/courseController";
+import multer from "multer";
+import { listCourses, getCourse, createCourse, updateCourse, deleteCourse } from "../controllers/courseController";
+import { requireAuth } from "@clerk/express";
 
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage()})
 
 router.get("/", listCourses);
+router.post("/", requireAuth(), createCourse);
 router.get("/:courseId", getCourse)
+router.put("/:courseId", requireAuth(), upload.single("image"), updateCourse);
+router.delete("/:courseId", requireAuth(), deleteCourse);
 
 export default router;
